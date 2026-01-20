d378601c1395407ae9bae51506a64fb75f29f5b6
wenke.lai <wenke.studio@gmail.com>
2026-01-20 15:00:11 +0800

    feat(infinity): add initial game page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="relative w-full">
+	<div class="container mx-auto">
+		<hgroup class="mb-8">
+			<h2 class="text-2xl uppercase">links</h2>
+		</hgroup>
+
+		<ul class="text-xl font-bold">
+			<li>
+				<a class="text-sky-600 hover:underline" href="/infinity">Infinity</a>
+			</li>
+		</ul>
+	</div>
+</section>
---
+++ src/routes/infinity/+page.svelte
@@ -0,0 +1,13 @@
+<section class="relative w-full">
+	<div class="container mx-auto">
+		<hgroup>
+			<h2 class="text-2xl uppercase">Infinity</h2>
+		</hgroup>
+	</div>
+</section>
+
+<section class="relative w-full">
+	<div class="container mx-auto">
+		<div class="game"></div>
+	</div>
+</section>
